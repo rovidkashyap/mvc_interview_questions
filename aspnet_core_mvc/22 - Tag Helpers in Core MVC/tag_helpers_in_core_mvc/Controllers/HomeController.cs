c6c4c12b6aefa67a60819992ce1c1ea52ef8ea6f
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-05 19:26:32 +0530

    Question 22 - Tag Helpers in Core MVC

---
+++ aspnet_core_mvc/22 - Tag Helpers in Core MVC/tag_helpers_in_core_mvc/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace tag_helpers_in_core_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    // Custom Tag Helper
+    [HtmlTargetElement("email-link")]
+    public class EmailTagHelper : TagHelper
+    {
+        public string Address { get; set; }
+        public string DisplayText { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";   // Replaces <email-link> with <a> tag
+            output.Attributes.SetAttribute("href", $"mailto:(Address)");
+            output.Content.SetContent(DisplayText);
+        }
+    }
+}
---
+++ aspnet_core_mvc/22 - Tag Helpers in Core MVC/tag_helpers_in_core_mvc/README.txt
@@ -0,0 +1,23 @@
+﻿
+Tag Helpers are a feature in ASP.NET Core MVC that allow you to add server-side logic to HTML elements 
+in a way that is more natural and HTML-friendly than traditional HTML helpers. 
+
+They are designed to make your views more readable and maintainable by enabling a more HTML-centric 
+approach to writing markup.
+
+Built-In Tag Helpers
+
+1. Form Tag Helper
+
+2. Input Tag Helper
+
+3. Label Tag Helper
+
+4. Validation Message Tag Helper
+
+5. Anchor Tag Helper
+
+6. Creating Custom Tag Helpers
+	- Create the Custom Tag Helper Class
+	- Register the Tag Helper in `_ViewImports.cshtml`
+	- Use the Custom Tag Helper in a View
