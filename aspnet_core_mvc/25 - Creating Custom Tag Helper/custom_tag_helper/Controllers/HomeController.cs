7ad84b0ac2b4eeeed9b7e9745dbc694dcbbf3bd1
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-05 19:05:41 +0530

    Question 25 - Custom HTML Helper in Core MVC

---
+++ aspnet_core_mvc/25 - Creating Custom Tag Helper/custom_tag_helper/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace custom_tag_helper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    // Creating Custom Tag Helper
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlContent CustomLabel(this IHtmlHelper htmlHelper, string text, string cssClass)
+        {
+            var tagBuilder = new TagBuilder("label");
+            tagBuilder.InnerHtml.Append(text);
+            tagBuilder.AddCssClass(cssClass);
+
+            return tagBuilder;
+        }
+    }
+
+    // HTML Helpers in ASP.Net Core MVC generate HTML markup for rendering views.
+
+    // Standard HTML Helpers create basic HTML elements and are not tied to model properties.
+
+    // Strongly-Types HTML Helpers are tied to model properties, providing better compile-time
+    // checking and IntelliSense support.
+
+    // Custom HTML Helpers can be created to encapsulate complex HTML generation logic.
+}
