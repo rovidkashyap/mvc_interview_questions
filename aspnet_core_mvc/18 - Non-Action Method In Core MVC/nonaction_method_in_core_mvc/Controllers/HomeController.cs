fd5cc6472c7a81ea9131293f5789937363945e50
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-05 17:31:23 +0530

    Question 18 - Non-Action Method in Core MVC

---
+++ aspnet_core_mvc/18 - Non-Action Method In Core MVC/nonaction_method_in_core_mvc/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nonaction_method_in_core_mvc.Controllers
+{
+    // In ASP.NET Core MVC, non-action methods are methods within a controller that are not meant to
+    // be treated as action methods. These methods are typically utility or helper methods that
+    // perform some kind of internal processing or logic that supports the controller's actions
+    // but should not be directly accessible via HTTP requests.
+
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var message = GetMessage();
+            ViewData["Message"] = message;
+
+            return View();
+        }
+
+        [NonAction]
+        public string GetMessage()
+        {
+            return "Hello, welcome to the Home page!";
+        }
+    }
+}
